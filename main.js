f148122bcead1c8b6436be2bb80ce25861cb68c0
valencarballo <valentincarballo99@gmail.com>
2022-04-13 14:42:13 -0300

    Primer entrega

--- main.js
+++ main.js
@@ -4,24 +4,26 @@ let promedio = 0
 let num = parseInt(prompt("Ingrese un numero:"))
 let mayor = num
 let menor = num
-
-while (num != 0 && num > 0){
-    suma = suma + num;
-    cont = cont +=1;
-    num = parseInt(prompt("Ingrese un numero (Para finalizar ingrese 0):"));
-    if (num > mayor){
-        mayor = num
-    };
-    if (num < menor && num != 0){
-        menor = num
+    
+    while (num != 0 && num > 0){
+        suma = suma + num;
+        cont = cont +=1;
+        num = parseInt(prompt("Ingrese un numero (Para finalizar ingrese 0):"));
+        if (num > mayor){
+            mayor = num
+        };
+        if (num < menor && num != 0){
+            menor = num
+        }
     }
-}
-if (num == 0){
-    promedio = suma/cont;
-    console.log("El promedio es:", promedio);
-    console.log("El mayor valor es:", mayor);
-    console.log("El menor valor es:", menor);
-    console.log("Cantidad de numeros", cont);
-}
+    if (num == 0){
+        promedio = suma/cont;
+        console.log("El promedio es:" + " " + promedio);
+        console.log("El mayor valor es:" + " " +mayor);
+        console.log("El menor valor es:" + " " + menor);
+        console.log("Cantidad de numeros:" + " " + cont);
+    }
+
+
     
     
